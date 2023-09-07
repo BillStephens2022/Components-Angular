8e7dd399d60b76ba72992f835c0d5b59a0c95ad1
BillStephens2022 <two4onebill@yahoo.com>
2023-09-07 14:28:29 -0400

    fixed types in table component

--- src/app/collections/table/table.component.ts
+++ src/app/collections/table/table.component.ts
@@ -6,7 +6,7 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./table.component.css']
 })
 export class TableComponent {
-@Input() data: [] = [];
-@Input() headers: [] = [];
+@Input() data: any = [];
+@Input() headers: any = [];
 @Input() classNames: string = '';
 }
