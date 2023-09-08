965ba0397183a1e077f5e0c0fcf5bf7d295c6c7d
BillStephens2022 <two4onebill@yahoo.com>
2023-09-08 07:43:25 -0400

    added open/close functionality to modal

--- src/app/mods/modal/modal.component.ts
+++ src/app/mods/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
@@ -6,10 +6,21 @@ import { Component, ElementRef, OnInit } from '@angular/core';
   styleUrls: ['./modal.component.css']
 })
 export class ModalComponent {
+  @Output() close = new EventEmitter();
+
   constructor(private el: ElementRef) {
     
   }
+
   ngOnInit() {
     document.body.appendChild(this.el.nativeElement);
+  }
+
+  ngOnDestroy() {
+    this.el.nativeElement.remove();
+  }
+
+  onCloseClick() {
+    this.close.emit();
   }
 }
